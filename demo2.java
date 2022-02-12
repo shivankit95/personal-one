45b75a36e93804375660c01048447b18a351141e
shivankit95 <60230229+shivankit95@users.noreply.github.com>
2022-02-12 18:33:39 +0530

    Update demo2.java

--- demo2.java
+++ demo2.java
@@ -3,6 +3,11 @@ public class demo2{
  static void display(){
 
  System.out.println("hello son of good mother get aaway from tha ATTENT!ON whore ");
+  System.out.println("hello mtoher fucekr welcome to coding");
+   public String custommethod(){
+   return "welcome coding";
+   
+   }
 
 }
 
